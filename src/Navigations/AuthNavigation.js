7ec72d08c30707bd8bd7daed1a71623d7dd2be71
0jiiino <tnqkrtnqkr7756@gmail.com>
2022-03-03 02:31:29 +0900

    feat: Add screen header and add login screen stack

--- src/Navigations/AuthNavigation.js
+++ src/Navigations/AuthNavigation.js
@@ -1,6 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import React from "react";
+import Login from "../screens/Login/LoginScreen";
 
 import Main from "../screens/Main/MainScreen";
 import SignUp from "../screens/SignUp/SignUpScreen";
@@ -11,10 +12,45 @@ const Stack = createStackNavigator();
 const AuthNavigation = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Main" component={Main} />
-        <Stack.Screen name="SignUp" component={SignUp} />
-        <Stack.Screen name="Success" component={Success} />
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Main"
+          component={Main}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="SignUp"
+          component={SignUp}
+          options={{
+            title: "🌈 회원가입",
+            headerStyle: {
+              backgroundColor: "#FFFDDD",
+            },
+            headerTintColor: "black",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+          }}
+        />
+        <Stack.Screen
+          name="Success"
+          component={Success}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{
+            title: "🌈 로그인",
+            headerStyle: {
+              backgroundColor: "#FFFDDD",
+            },
+            headerTintColor: "black",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
