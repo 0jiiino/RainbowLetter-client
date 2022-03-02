cb8fce301a2e337779ea19103e8ba081b60b327f
0jiiino <tnqkrtnqkr7756@gmail.com>
2022-03-02 12:54:45 +0900

    docs: no-code change

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,5 +23,15 @@ module.exports = {
     "prettier/prettier": "error",
     "no-use-before-define": "off",
     "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+    "react/function-component-definition": "off",
+    "react/prop-types": "off",
+    "react/jsx-boolean-value": "off",
+    "import/no-unresolved": "off",
+    "no-return-await": "off",
+    "import/prefer-default-export": "off",
+    "consistent-return": "off",
+  },
+  globals: {
+    fetch: false,
   },
 };
