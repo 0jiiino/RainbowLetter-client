b9269659c91ca09e0be6477d7ff771731b36ede5
0jiiino <tnqkrtnqkr7756@gmail.com>
2022-03-04 03:51:37 +0900

    feat: Implement new angel creation logic using thunk

--- redux/userSlice.js
+++ redux/userSlice.js
@@ -1,9 +1,29 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+import { postNewAngel } from "../api/letter.Api";
+
+export const fetchNewAngel = createAsyncThunk(
+  "angel",
+  async (creationInfo, thunkAPI) => {
+    try {
+      const response = await postNewAngel(creationInfo);
+
+      if (!response.error) {
+        return response;
+      }
+
+      return thunkAPI.rejectWithValue(response);
+    } catch (error) {
+      thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
 
 const initialState = {
   id: "",
   nickname: "",
-  angel: [],
+  angel: {},
+  message: "",
   isLoading: false,
 };
 
@@ -14,14 +34,30 @@ export const userSlice = createSlice({
     login: (state, action) => {
       const { user } = action.payload;
 
+      state.id = user.id;
+      state.nickname = user.nickname;
+
       if (user?.angel) {
         state.angel = user.angel;
       }
 
-      state.id = user.id;
-      state.nickname = user.nickname;
-
       return state;
+    },
+  },
+  extraReducers: {
+    [fetchNewAngel.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [fetchNewAngel.fulfilled]: (state, { payload }) => {
+      const { angel } = payload;
+
+      state.angel = angel;
+    },
+    [fetchNewAngel.rejected]: (state, { payload }) => {
+      const { error } = payload;
+
+      state.message = error.message;
+      state.isLoading = false;
     },
   },
 });
