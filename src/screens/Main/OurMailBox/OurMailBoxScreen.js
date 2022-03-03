c69d3237e726afde5dc800c5e6db6c707aad9db3
0jiiino <tnqkrtnqkr7756@gmail.com>
2022-03-04 03:46:34 +0900

    feat: Implementation of the first screen of everyone's mailbox

---
+++ src/screens/Main/OurMailBox/OurMailBoxScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const OurMailBox = () => {
+  return (
+    <View>
+      <Text>흠-?!?!</Text>
+    </View>
+  );
+};
+
+export default OurMailBox;
