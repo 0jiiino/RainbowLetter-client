5e13e38949d8b1344916ac72bf1746622a0a4fc5
0jiiino <tnqkrtnqkr7756@gmail.com>
2022-03-05 02:45:54 +0900

    feat: Implementation of logic to manage the global state of the received mail list

--- redux/userSlice.js
+++ redux/userSlice.js
@@ -1,6 +1,6 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-import { postNewAngel } from "../api/letter.Api";
+import { getAngelLetters, postNewAngel } from "../api/letter.Api";
 
 export const fetchNewAngel = createAsyncThunk(
   "angel",
@@ -19,12 +19,30 @@ export const fetchNewAngel = createAsyncThunk(
   }
 );
 
+export const fetchLetters = createAsyncThunk(
+  "letters",
+  async (id, thunkAPI) => {
+    try {
+      const response = await getAngelLetters(id);
+
+      if (!response.error) {
+        return response;
+      }
+
+      return thunkAPI.rejectWithValue(response);
+    } catch (error) {
+      thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
 const initialState = {
   id: "",
   nickname: "",
   angel: {},
   message: "",
   isLoading: false,
+  letters: [],
 };
 
 export const userSlice = createSlice({
@@ -52,8 +70,24 @@ export const userSlice = createSlice({
       const { angel } = payload;
 
       state.angel = angel;
+      state.isLoading = false;
     },
     [fetchNewAngel.rejected]: (state, { payload }) => {
+      const { error } = payload;
+
+      state.message = error.message;
+      state.isLoading = false;
+    },
+    [fetchLetters.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [fetchLetters.fulfilled]: (state, { payload }) => {
+      const { letters } = payload;
+
+      state.letters = letters;
+      state.isLoading = false;
+    },
+    [fetchLetters.rejected]: (state, { payload }) => {
       const { error } = payload;
 
       state.message = error.message;
