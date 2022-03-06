7c0c21420749e89b305b02454220855575d25043
0jiiino <tnqkrtnqkr7756@gmail.com>
2022-03-07 05:02:55 +0900

    feat: Implement custom hook useEffect not executed on first render

---
+++ src/utils/useDidMountEffect.js
@@ -0,0 +1,16 @@
+import { useEffect, useRef } from "react";
+
+const useDidMountEffect = (func, deps) => {
+  const didMount = useRef(false);
+
+  useEffect(() => {
+    if (didMount.current) {
+      func();
+      return;
+    }
+
+    didMount.current = true;
+  }, [deps]);
+};
+
+export default useDidMountEffect;
