4b006d1b6bff92c47406dab0b965cd8caf1ad3e9
0jiiino <tnqkrtnqkr7756@gmail.com>
2022-03-08 02:26:13 +0900

    feat: Implement a function that asks the server for a list of mails with echo turned on

---
+++ api/letterApi.js
@@ -0,0 +1,37 @@
+import { SERVER_URI } from "@env";
+import * as SecureStore from "expo-secure-store";
+
+export const patchEcho = async (echoInfo) => {
+  const accessToken = (
+    await SecureStore.getItemAsync("accessToken")
+  ).replaceAll('"', "");
+
+  const { echo, id } = echoInfo;
+
+  const response = await fetch(`${SERVER_URI}/letters/${id}/echos`, {
+    method: "PATCH",
+    headers: {
+      Authorization: accessToken,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ echo }),
+  });
+
+  return await response.json();
+};
+
+export const getEchoLetters = async () => {
+  const accessToken = (
+    await SecureStore.getItemAsync("accessToken")
+  ).replaceAll('"', "");
+
+  const response = await fetch(`${SERVER_URI}/letters`, {
+    method: "GET",
+    headers: {
+      Authorization: accessToken,
+      "Content-Type": "application/json",
+    },
+  });
+
+  return await response.json();
+};
