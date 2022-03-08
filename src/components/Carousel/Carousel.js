b7a282104ba83115ae196a5864b375cbb7aee421
0jiiino <tnqkrtnqkr7756@gmail.com>
2022-03-09 06:54:22 +0900

    feat: Implement carousel component

---
+++ src/components/Carousel/Carousel.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { FlatList } from "react-native";
+
+import { WINDOW_HEIGHT, WINDOW_WIDTH } from "../../constants/constants";
+
+const Carousel = ({ data, renderItem }) => {
+  const gap = WINDOW_WIDTH * 0.025;
+  const offset = WINDOW_WIDTH * 0.075;
+  const pageWidth = WINDOW_WIDTH * 0.8;
+
+  return (
+    <FlatList
+      horizontal={true}
+      contentContainerStyle={{
+        paddingHorizontal: gap + offset / 2,
+        marginVertical: WINDOW_HEIGHT * 0.05,
+      }}
+      data={data}
+      renderItem={renderItem}
+      snapToInterval={pageWidth + gap}
+      keyExtractor={(item) => (item._id ? item._id : 0)}
+    />
+  );
+};
+
+export default Carousel;
